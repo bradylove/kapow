26053abaf9759ffa1acd1e0c4d77e6ace4e6a552
Brady Love <love.brady@gmail.com>
2011-05-23 17:39:49 -0700

    status bar menu working

--- KaPow/AppDelegate.rb
+++ KaPow/AppDelegate.rb
@@ -9,6 +9,7 @@ require 'FileUtils'
 
 class AppDelegate
   attr_accessor :window
+  attr_accessor :mainMenu
   attr_accessor :browseButton, :saveButton, :urlButton, :restartButton
   attr_accessor :alwaysRestartCheckbox
   attr_accessor :appNameField, :appPathField
@@ -17,13 +18,17 @@ class AppDelegate
   POWDIR = File.expand_path '~/.pow/'
 
   def applicationDidFinishLaunching(a_notification)
+    # self.menuBarVisible = false
+
     @link_control = LinkControl.new
-    @status_bar = StatusBar.new
-    @status_bar.initStatusBar(@apps)
+    self.initStatusBar
     
     self.get_current_apps
     self.enable_fields
   end
+
+
+
 
   def browse(sender)
     dialog = NSOpenPanel.openPanel
@@ -51,7 +56,7 @@ class AppDelegate
     end
 
     @appListTableView.reloadData
-    @status_bar.setup_menu(@apps)
+    self.setup_menu
   end
 
   def numberOfRowsInTableView(view)
@@ -85,7 +90,7 @@ class AppDelegate
 
       @apps << new_app
       @appListTableView.reloadData
-      @status_bar.setup_menu(@apps)
+      self.setup_menu
 
       self.clear_fields
     else
@@ -192,6 +197,73 @@ class AppDelegate
     else
       @link_control.remove_always_restart(f)
     end
+  end
+
+
+  #################################
+  # Status Bar
+  #################################
+
+  def initStatusBar
+    status_bar = NSStatusBar.systemStatusBar
+    @statusBarItem = status_bar.statusItemWithLength(NSVariableStatusItemLength)
+    
+    image = NSImage.imageNamed 'system-icon'
+    @statusBarItem.setImage image
+
+    self.setup_menu
+  end
+
+  def setup_menu
+    menu = NSMenu.new
+    menu.initWithTitle 'KaPow'
+    mi = NSMenuItem.new
+    mi.title = 'Open KaPow'
+    mi.action = 'open_kapow:'
+    mi.target =  self
+    menu.addItem mi
+
+    mi = NSMenuItem.separatorItem
+
+    menu.addItem mi
+    
+    @apps.each do |app|
+      menu.addItem(NSMenuItem.new.tap do |menu_item|
+        menu_item.title = app.name
+        menu_item.setSubmenu(NSMenu.new.tap do |sub_menu|
+          smi = NSMenuItem.new
+          smi.title = 'Open in Browser'
+          smi.action = 'go_to_app_click:'
+          smi.representedObject = app.name
+          smi.target = self
+          sub_menu.addItem smi
+
+          smi = NSMenuItem.new
+          smi.title = 'Restart App'
+          smi.action = 'restart_server_from_menu:'
+          smi.representedObject = app.target
+          smi.target = self
+          sub_menu.addItem smi
+        end)
+      end)
+
+    end
+
+    menu
+
+    @statusBarItem.setMenu menu
+  end
+
+  def go_to_app_click(sender)
+     go_to_app("http://" + sender.representedObject + ".dev")
+  end
+
+  def restart_server_from_menu(sender)
+    @link_control.restart(sender.representedObject.to_s)
+  end
+
+  def open_kapow(sender)
+    @window.isVisible = true
   end
 end
 
